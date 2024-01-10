a09fcb393924c33aec4c1ec6a36c030131a43072
Abdulaziz <abizlancer@gmail.com>
2024-01-10 13:00:41 +0500

    feat: change structure of client page and card

--- src/views/Student.vue
+++ src/views/Student.vue
@@ -1,19 +1,24 @@
 <template>
-  <Client />
+  <div class="client">
+    <ClientBanner clientCategory="student" />
+    <div class="client-content container flex flex-col items-center gap-10">
+      <ClientCard clientCategory="student" />
+      <img src="../assets/images/client-bg.png" class="client-bg" alt="client-item" >
+    </div>
+  </div>
 </template>
 
 <script setup lang="ts">
-import Client from '@/components/client/Client.vue';
+import ClientBanner from '@/components/client/ClientBanner.vue';
+import ClientCard from '@/components/client/ClientCard.vue';
 
 import { useRoute } from 'vue-router';
 
 import { useClientStore } from '@/stores/client';
 
 const route = useRoute()
-const { name, params } = route
-console.log(route);
 
-const client = useClientStore()
-client.getClientInfo(name, params.id)
+const clientStore = useClientStore()
+clientStore.getClientInfo('student', route.params.id)
 
 </script>
